38dcedad8170c3b825a167085dcabe4be4b7dd05
vitalii-kyiv <v.s.chernous@gmail.com>
2023-12-09 23:23:22 +0200

    final_0.1

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,47 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number = Math.random()) {}
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-house.openDoor(person.getKey());
+class Person {
+  constructor(private key: Key = new Key()) {}
 
-house.comeIn(person);
+  getKey(): Key {
+    return this.key;
+  }
+}
 
+abstract class House {
+  protected tenants: Person[] = [];
+
+ constructor(protected key: Key = new Key(), protected door: boolean = false) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    } 
+  }
+
+  OpenDoor(key: Key): void{};
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    } 
+  }
+}
+
+const myKey = new Key();
+const person = new Person(myKey);
+const myHouse = new MyHouse(myKey);
+
+
+myHouse.comeIn(person);
+myHouse.openDoor(person.getKey());
 
 export {};
